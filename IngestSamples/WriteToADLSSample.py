4cb765d87e018b3d701669d8dd1d7ec2dd456ef9
dfroslie-ndsu <111405021+dfroslie-ndsu@users.noreply.github.com>
2022-09-14 11:57:10 -0500

    Add writing to ADLS sample

--- IngestSamples/WriteToADLSSample.py
+++ IngestSamples/WriteToADLSSample.py
@@ -3,17 +3,13 @@
 # and a local file, ActiveAstronauts.csv exists.
 
 # You may need to install the following packages:
-#   pip install pandas
+#   pip install 
+#   pip install azure-storage-file-datalake
 import pandas as pd
 from azure.storage.filedatalake import DataLakeServiceClient
 
 print("Hello world")  
 
-#%%
-# Get current space station position
-astros_df = pd.read_csv("ActiveAstronauts.csv")
-
-print(astros_df)
 
 # %%
 # Method to connect to a storage account with an account key.
@@ -43,10 +39,21 @@ initialize_storage_account(storage_account_name, storage_account_key)
 print(service_client)
 
 #%%
+# Create a container and a directory.
+file_system_client = service_client.create_file_system(file_system="assign-2-sample2")    
+
+directory_client=file_system_client.create_directory("Astronauts2")
 
 
+#%%
+directory_client = file_system_client.get_directory_client("Astronauts")
+        
+file_client = directory_client.create_file("ActiveAstronauts.csv")
 
+astronauts_file = open("ActiveAstronauts.csv",'r')
 
+file_contents = astronauts_file.read()
 
+file_client.upload_data(file_contents, overwrite=True)
 
 # %%

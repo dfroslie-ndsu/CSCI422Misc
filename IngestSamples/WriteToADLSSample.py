617407f60aa59696ee2f39fb1989fa00190267d0
dfroslie-ndsu <111405021+dfroslie-ndsu@users.noreply.github.com>
2022-09-14 11:11:34 -0500

    Move account key to file

--- IngestSamples/WriteToADLSSample.py
+++ IngestSamples/WriteToADLSSample.py
@@ -3,35 +3,50 @@
 # and a local file, ActiveAstronauts.csv exists.
 
 # You may need to install the following packages:
-#   pip install requests
 #   pip install pandas
-import requests
-import json
 import pandas as pd
+from azure.storage.filedatalake import DataLakeServiceClient
 
 print("Hello world")  
 
 #%%
 # Get current space station position
-api_response = requests.get(
-    "http://api.open-notify.org/iss-now.json"
-)
-
-print(api_response.content)
-
-# %%
-# Get current number of people in space
-api_response = requests.get(
-    "http://api.open-notify.org/astros.json" 
-)
-
-# Parse the result and convert the people list to a Pandas dataframe.
-json_data = json.loads(api_response.content)
-
-if json_data['message']=="success":
-    astro_count = json_data['number']
-    astros_df = pd.DataFrame(json_data['people'])
+astros_df = pd.read_csv("ActiveAstronauts.csv")
 
 print(astros_df)
 
+# %%
+# Method to connect to a storage account with an account key.
+# For this and other account connection approaches, see https://docs.microsoft.com/en-us/azure/storage/blobs/data-lake-storage-directory-file-acl-python
+def initialize_storage_account(storage_account_name, storage_account_key):
+    
+    try:  
+        global service_client
 
+        service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+            "https", storage_account_name), credential=storage_account_key)
+    
+    except Exception as e:
+        print(e)
+
+
+#%%
+storage_account_name = "assign1storage"
+
+# Note - store file in a config file that is NOT checked in.
+# Use .gitignore to avoid accidental submission.
+with open("AccountKey.config") as f:
+    storage_account_key=f.readline()
+
+initialize_storage_account(storage_account_name, storage_account_key)
+
+print(service_client)
+
+#%%
+
+
+
+
+
+
+# %%
